335458a4cd0e3dfaac6bf7dfbb79e00b09fa2bc9
RickyBv1 <rickycortes668668@gmail.com>
2025-03-02 17:34:21 -0500

    add audio feedback for scoring and hitting; refactor paddle and ball classes for improved functionality and CPU control

--- index.js
+++ index.js
@@ -1,55 +1,84 @@
 const gameZone = document.getElementById("gameZone");
 let ball;
+const helpMargin = 10;
 const messageElement = document.getElementById("message");
-const instructionsElement = document.getElementById("instructions")
-gameStatus = "PAUSE";
+let gameStatus = "PLAY";
+const audioScore = new Audio("audio/score.mp3");
+const audioHit = new Audio("audio/hit.mp3");
+const instructionsElement = document.getElementById("instructions");
 
 class Pallette {
-  element;
-  y = 0;
-  speed = 15;
-  movement;
+  y;
+  x;
   tall = 200;
   width = 20;
+  element;
   cpu;
+  movement;
+  speed = 15;
 
-  constructor() {
+  constructor(id) {
     this.element = document.createElement("div");
     this.element.classList = "pallette";
-    gameZone.children[0].appendChild(this.element);
+    this.element.id = id;
+    this.element.style.height = this.tall + "px"
+    this.element.style.width = this.width + "px"
+    gameZone.appendChild(this.element)
     this.resetPosition();
-  }
-
-  up() {
-    if (!this.movement) {
-      this.movement = setInterval(() => {
-        this.y -= this.speed;
-        if (this.y < 0) {
-          this.y = 0;
-          this.freeze();
-        }
-        this.element.style.top = this.y + "px";
-      }, 20);
-    }
   }
 
   down() {
     if (!this.movement) {
       this.movement = setInterval(() => {
         this.y += this.speed;
-        const limit = document.body.clientHeight - this.tall;
-        if (this.y > limit) {
-          this.y = limit;
-          this.freeze();
-        }
+        if (this.y > document.body.clientHeight - this.tall)
+          this.y = document.body.clientHeight - tthis.tall;
+        this.element.style.top = this.y + "px"
+      }, 10);
+    }
+  }
+
+  up() {
+    if (!this.movement) {
+      this.movement = setInterval(() => {
+        this.y += this.speed * -1;
+        if(this.y < 0) this.y + 0;
         this.element.style.top = this.y + "px";
-      }, 20);
+      }, 10);
     }
   }
 
   freeze() {
-    clearInterval(this.movement);
-    this.movement = undefined;
+    if(this.movement) {
+      clearInterval(this.movement);
+      this.movement = false;
+    }
+  }
+
+  toggleCPU(disable = false) {
+    if (!this.cpu && disable === false) {
+      this.freeze();
+      this.cpu = setInterval(() => {
+        if(ball) {
+          const palletteCenter = this.y + this.tall / 2;
+          this.freeze();
+          if(
+            Math.abs(ball.y + this.width / 2 - palletteCenter) < 
+            this.width / 2
+          ) {}
+          else if (ball.y + this.width / 2 < palletteCenter) {
+            this.up();
+          } else {
+            this.down();
+          }
+        } else this.resetPosition();
+      }, 40);
+    } else {
+      clearInterval(this.movement);
+      this.movement = false;
+      clearInterval(this.cpu);
+      this.cpu = false;
+    }
   }
 
   resetPosition() {
@@ -58,98 +87,77 @@ class Pallette {
     this.element.style.top = this.y + "px";
   }
 
-  toggleCPU() {
-    if(this.cpu) {
-      clearInterval(this.cpu)
-      clearInterval(this.movement)
-      this.movement = undefined;
-      this.cpu = undefined
-    } else {
-      this.cpu = setInterval(() => {
-        if(!ball) return;
-        if(ball.getCenter() < this.y + this.tall / 3 &&
-          ball.getCenter() > this.y + this.tall / 3 * 2
-        ) {
-          this.freeze();
-        }
-        else if(ball.getCenter() < this.getCenter()) {
-          this.up();
-        }
-        else if(ball.getCenter() > this.getCenter()) {
-          this.down();
-        }
-      }, 20)
-    }
-  }
-
   getCenter() {
-    return this.y + this.tall / 2
+    return this.y + this.tall / 2;
   }
-
 }
 
 class Ball {
   x;
   y;
+  width = 30;
   xd = -15;
   yd = 0; //"d" means differential
-  width = 30;
+  element;
   movement;
 
   constructor() {
     this.element = document.createElement("div");
     this.element.classList = "ball";
     gameZone.appendChild(this.element);
+    this.element.style.width = this.width + "px";
     this.resetPosition();
     this.move();
-    messageElement.classList = "hidden";
-    instructionsElement.classList.toggle("hidden" ,true);
-  }
-
-  resetPosition() {
-    this.x = document.body.clientWidth / 2 - this.width / 2;
-    this.element.style.left = this.x + "px";
-    this.y = document.body.clientHeight / 2 - this.width / 2;
-    this.element.style.top = this.y + "px";
   }
 
   move() {
     if (!this.movement) {
       this.movement = setInterval(() => {
-        //horizontal movement
         this.x += this.xd;
+        this.y += this.yd;
 
-        //pallette impact
-        //pallette p1
+        //Pallette
+        //Pallette p1
         if (
           this.x < 0 + p1.width &&
-          this.y + this.width / 2 > p1.y &&
-          this.y + this.width / 2 < p1.y + p1.tall
+          this.getCenter() + helpMargin > p1.y &&
+          this.y - this.width / 2 - helpMargin < p1.y + p1.tall
         ) {
           this.yd += this.yVariation(p1);
           this.xd = this.xd * -1;
+          this.x += this.xd;
+          audioHit.play();
         }
         //pallette p2
-        if (
-          this.x + this.width > document.body.clientWidth - p2.width &&
-          this.y + this.width / 2 > p2.y &&
-          this.y + this.width / 2 < p2.y + p2.tall
+        else if (
+          this.x > document.body.clientWidth - p2.width - this.width &&
+          this.getCenter() + helpMargin > p2.y &&
+          this.y - this.width / 2 - helpMargin < p2.y + p1.tall
         ) {
           this.yd += this.yVariation(p2);
           this.xd = this.xd * -1;
+          this.x += this.xd;
+          audioHit.play();
         }
 
         //add a point
-        if (this.x < 0 || this.x > document.body.clientWidth - this.width) {
-          board.add(this.x < 100 ? 2 : 1);
+        else if(
+          this.x < 0 ||
+          this.x > document.body.clientWidth - this.width
+        ) {
+          if(this.x < 100) addPoint(2);
+          else addPoint(1);
+          this.resetPosition();
+          
         }
-        this.element.style.left = this.x + "px";
 
         //vertical movement
-        this.y += this.yd;
-        if (this.y < 0 || this.y > document.body.clientHeight - this.width) {
+        if(this.y < 0 || this.y > document.body.clientHeight - this.width) {
           this.yd = this.yd * -1;
+          this.y += this.yd
         }
+
+        this.element.style.left = this.x + "px";
         this.element.style.top = this.y + "px";
       }, 20);
     }
@@ -157,8 +165,15 @@ class Ball {
 
   delete() {
     clearInterval(this.movement);
+    gameStatus = "PAUSE"
     gameZone.removeChild(this.element);
-    ball = undefined;
+  }
+
+  resetPosition() {
+    this.x = document.body.clientWidth / 2 - this.width / 2;
+    this.y = document.body.clientHeight / 2 - this.width / 2;
+    this.element.style.left = this.x + "px";
+    this.element.style.top = this.y + "px";
   }
 
   yVariation(p) {
@@ -167,58 +182,55 @@ class Ball {
   }
 
   getCenter() {
-    return this.y + this.width / 2
+    return this.y + this.width / 2;
   }
-
 }
 
 class Board {
+  p1Score = 0;
+  p2Score = 0;
+  maxScore = 6;
 
-    p1Score = 0;
-    p2Score = 0;
-    maxScore = 6;
+  constructor() {
+    this.element = document.createElement("p");
+    this.element.id = "board";
+    gameZone.appendChild(this.element);
+    this.updateText();
+  }
 
-    constructor() {
-        this.element = document.createElement("p");
-        this.element.id = "board";
-        gameZone.appendChild(this.element);
-        this.updateText();
-      }
+  updateText() {
+    this.element.textContent = this.p1Score + " - " + this.p2Score;
+  }
 
-      updateText() {
-        this.element.textContent = this.p1Score + " - " + this.p2Score;
-      }
+  add(p) {
+    if (p === 1) this.p1Score++;
+    else this.p2Score++;
+    this.updateText();
+    ball.delete();
+    p1.resetPosition();
+    p2.resetPosition();
+    messageElement.textContent = 'press "space" to continue';
+    messageElement.classList.toggle("hidden", false);
+    this.gameStatus = "PAUSE";
+    if (this.p1Score >= this.maxScore) {
+      this.win(1);
+    } else if (this.p2Score >= this.maxScore) {
+      this.win(2);
+    }
+  }
 
-      add(p){
-        if(p === 1) this.p1Score++
-        else this.p2Score++
-        this.updateText();
-        ball.delete();
-        p1.resetPosition();
-        p2.resetPosition();
-        messageElement.textContent = 'press "space" to continue';
-        messageElement.classList.toggle("hidden", false);
-        this.gameStatus = "PAUSE";
-        if(this.p1Score >= this.maxScore) {
-          this.win(1)
-        } else if(this.p2Score >= this.maxScore) {
-          this.win(2)
-        }
-      }
+  win(p) {
+    messageElement.textContent = "Player " + p + " win!";
+    messageElement.classList.toggle("spark", true);
+    gameStatus = "END";
+  }
 
-      win(p) {
-        messageElement.textContent = 'Player ' + p + ' win!'
-        messageElement.classList.toggle("spark", true);
-        gameStatus = "END"
-      }
-
-      reset() {
-        this.p1Score = 0;
-        this.p2Score = 0;
-        this.updateText();
-        messageElement.classList.toggle("spark", false);
-      }
-
+  reset() {
+    this.p1Score = 0;
+    this.p2Score = 0;
+    this.updateText();
+    messageElement.classList.toggle("spark", false);
+  }
 }
 
 document.addEventListener("keydown", (e) => {
@@ -242,8 +254,8 @@ document.addEventListener("keydown", (e) => {
       p2.toggleCPU();
       break;
     case " ":
-      if(gameStatus === "END") board.reset();
-      if(!ball) ball = new Ball();
+      if (gameStatus === "END") board.reset();
+      if (!ball) ball = new Ball();
       gameStatus = "PLAY";
       break;
   }
@@ -263,6 +275,6 @@ document.addEventListener("keyup", (e) => {
   }
 });
 
-const p1 = new Pallette();
-const p2 = new Pallette();
 const board = new Board();
+const p1 = new Pallette("pallette1");
+const p2 = new Pallette("pallette2");
